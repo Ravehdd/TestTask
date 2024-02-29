d3d0db70e3587d414aa015572f617ae2c8e20377
max <max.ershov.spb@gmail.com>
2024-02-29 17:26:29 +0300

    прописал струткуру бд, +- настроил админ панель, настроил авторизацию

---
+++ djsite/products/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.utils.safestring import mark_safe
+
+from .models import *
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "start_time", "max_users", "min_users")
+
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ("name", "video_link", "product_id")
+
+    # def video_link(self, obj):
+    #     return mark_safe(f'<a href="{obj.video_link}" target="_blank">{obj.video_link}</a>')
+    # video_link.short_description = 'Link'
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Lesson, LessonAdmin)
---
+++ djsite/products/models.py
@@ -0,0 +1,36 @@
+
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255, null=False)
+    start_time = models.DateTimeField(null=False, blank=True)
+    price = models.FloatField(null=False)
+    max_users = models.IntegerField(null=False)
+    min_users = models.IntegerField(null=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=255, null=False)
+    video_link = models.URLField(null=False, max_length=512)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=255, null=False)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+
+
+class GroupUsers(models.Model):
+    group = models.ForeignKey(Group, on_delete=models.PROTECT)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+
+
+class Access(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    is_allowed = models.BooleanField(default=False)
